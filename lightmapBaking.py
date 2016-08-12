0720027b290062443386b79b6d23ed92f0dd2e22
Joke van Oijen <joke.vanoijen@gmail.com>
2016-08-12 13:14:10 +0200

    Update lightmapBaking.py

--- lightmapBaking.py
+++ lightmapBaking.py
@@ -30,7 +30,7 @@ class JokePluginPolygons(bpy.types.Operator):
     def invoke(self, context, event):
         bpy.ops.object.mode_set(mode = 'EDIT')
         bpy.ops.mesh.select_all(action='DESELECT')
-        bpy.ops.mesh.select_face_by_sides(type='GREATER')
+        bpy.ops.mesh.select_face_by_sides(number=4, type='GREATER')
         bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
         bpy.ops.mesh.tris_convert_to_quads()
         bpy.ops.object.mode_set(mode = 'OBJECT')
